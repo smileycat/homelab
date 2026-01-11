17c25b520ea3c06e597098c29a02f0a3b22961d0
smileycat <liaocrong@outlook.com>
2026-01-11 08:35:42 +0000

    update immich backup

--- moon/scripts/immich_backup.sh
+++ moon/scripts/immich_backup.sh
@@ -3,14 +3,17 @@ docker_vol=/mnt/data/docker-volume
 # src1=$docker_vol/immich/library
 # src2=$docker_vol/immich/upload
 # src3=$docker_vol/immich/profile
-dest=$docker_vol/immich/backup
+dest=$docker_vol/immich/backups
 date=$(date +%Y-%m-%d)
 dbbackupPath=$dest/postgres-$date.sql.gz
 
 # mkdir -p $dest
 
 # backup immich db
-docker exec -t immich-postgres pg_dumpall -c -U postgres | gzip >"$dbbackupPath"
+docker exec -t immich-postgres pg_dumpall \
+    --clean \
+    --if-exists \
+    --username=postgres | gzip >"$dbbackupPath"
 # backup immich assets
 # rsync -av --delete $src1 $src2 $src3 $dest
 
@@ -19,7 +22,7 @@ docker exec -t immich-postgres pg_dumpall -c -U postgres | gzip >"$dbbackupPath"
 # docker compose create   # Create Docker containers for Immich apps without running them.
 # docker start immich-postgres    # Start Postgres server
 # sleep 10    # Wait for Postgres server to start up
-# gunzip < "/mnt/data/docker-volume/immich/backup/postgres-yyyy-mm-dd.sql.gz" \
-# | sed "s/SELECT pg_catalog.set_config('search_path', '', false);/SELECT pg_catalog.set_config('search_path', 'public, pg_catalog', true);/g" \
-# | docker exec -i immich-postgres psql --username=postgres    # Restore Backup
+# gunzip --stdout "$dbbackupPath" \
+#     | sed "s/SELECT pg_catalog.set_config('search_path', '', false);/SELECT pg_catalog.set_config('search_path', 'public, pg_catalog', true);/g" \
+#     | docker exec -i immich_postgres psql --dbname=postgres --username=postgres  # Restore Backup
 # docker compose up -d    # Start remainder of Immich apps
